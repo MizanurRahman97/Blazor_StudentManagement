df45d182ef4090f3da5b7d9345df348bb664243b
Md. Mizanur Rahman <140915455+rmizan863@users.noreply.github.com>
2023-11-23 12:01:36 +0600

    Add project files.

---
+++ StudentManagement/Server/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.Shared;
+
+namespace StudentManagement.Server.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
---
+++ StudentManagement/Shared/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Shared
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public DateTime AdmissionDate { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; }
+
+        public string? ImageUrl { get; set; }
+        public List<Address> Addresses { get; set; } = new List<Address>();
+    }
+}
